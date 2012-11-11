3d5fbc2434fdedf5822e6ab0688b79aa7cf00df6
Andrei Fangli <andrei_fangli@yahoo.com>
2012-11-11 18:38:53 +0200

    Adaug BD lab3 (pb 10 corectata, pb 11 - 20 rezolvare)

--- BD/Lab3/Selects.sql
+++ BD/Lab3/Selects.sql
@@ -131,7 +131,7 @@ select nota
 --10. Studentii care au note la toate disciplinele din planul de invatamant al sectiei din care fac parte.
 select *
     from studenti
-    where 0 <>(
+    where 0 =(
         select COUNT(*)
             from rezultate right join(
                 select codd
@@ -145,28 +145,202 @@ select *
 
 --11. Pentru fiecare sectie, numele studentului cu media cea mai mare, si media (aceeasi conditie asupra
 --    mediei ca la punctul 5).
+select sectii.denumires, medii.nume, medii.media
+    from sectii inner join(
+        select studenti.*, AVG(nota) as media
+            from studenti inner join rezultate
+                on studenti.nrmatricol = rezultate.nrmatricol
+            where 5 < all(
+                select nota
+                    from Rezultate
+                    where rezultate.nrmatricol = studenti.nrmatricol
+            )
+            group by studenti.nrmatricol, nume, cods, grupa, datan, nota
+    ) as medii
+    on sectii.cods = medii.cods
+    where medii.media >= all(
+        select AVG(nota)
+            from studenti inner join rezultate
+                on studenti.nrmatricol = rezultate.nrmatricol
+            where 5 < all(
+                select nota
+                    from Rezultate
+                    where rezultate.nrmatricol = studenti.nrmatricol
+            )
+            group by studenti.nrmatricol, nume, cods, grupa, datan, nota
+            having cods = medii.cods
+    )
 
 --12. Pentru fiecare disciplina, sa se afiseze notele (*) si numele studentilor care au primit acele note
 --    ((*) – notele care sunt mai mari decat media notelor la disciplina respectiva).
+select discipline.denumired, rez1.nota, studenti.nume
+    from discipline inner join(
+        select r1.disciplina, r1.nrmatricol, r1.nota
+            from rezultate as r1
+            where
+                r1.nota >(
+                    select AVG(nota)
+                    from rezultate as r2
+                        where r1.disciplina = r2.disciplina
+                )
+    ) as rez1
+    on discipline.codd = rez1.disciplina
+    inner join studenti
+    on rez1.nrmatricol = studenti.nrmatricol
 
 --13. Studentii care au nota cea mai mica la o disciplina si nota cea mai mare la o alta disciplina, notele
 --    respective si denumirea disciplinelor. Structura rezultatului va fi : Nume_student, Nota1, Disciplina1,
 --    Nota2, Disciplina2. A se elimina duplicatele. (Nu afisati “Ionescu, 4, BD, 10, SO” si “Ionescu, 10,
 --    SO, 4, BD” – e aceeasi informatie. Afisati doar una din aceste inregistrari.)
+select rez1.nume, rez1.denumired, rez1.nota, rez2.denumired, rez2.nota
+    from (
+        select s1.nume, s1.nrmatricol, d1.denumired, r1.nota, r1.disciplina
+            from studenti as s1 inner join rezultate as r1
+                on s1.nrmatricol = r1.nrmatricol
+                inner join discipline as d1
+                on r1.disciplina = d1.codd 
+            where nota <= all(
+                select nota
+                    from rezultate as r3
+                    where r3.disciplina = r1.disciplina
+            )
+    ) as rez1 inner join(
+        select s2.nume, s2.nrmatricol, d2.denumired, r2.nota, r2.disciplina
+            from studenti as s2 inner join rezultate as r2
+                on s2.nrmatricol = r2.nrmatricol
+                inner join discipline as d2
+                on r2.disciplina = d2.codd 
+            where nota >= all(
+                select nota
+                    from rezultate as r4
+                    where r4.disciplina = r2.disciplina
+            )
+    ) as rez2
+    on
+        rez1.nrmatricol = rez2.nrmatricol
+        and
+        rez1.disciplina <> rez2.disciplina
 
 --14. Primii 5 studenti din fiecare grupa, in ordinea descrescatoare a mediilor.
+select s1.*, rez1.media
+    from studenti as s1 inner join(
+        select nrmatricol, avg(nota) as media
+            from rezultate
+            group by nrmatricol
+    )as rez1
+    on s1.nrmatricol = rez1.nrmatricol
+    where rez1.media in(
+        select top 5 with ties avg(nota) as medie
+            from rezultate inner join studenti as s2
+            on rezultate.nrmatricol = s2.nrmatricol
+            where s2.grupa = s1.grupa
+            group by rezultate.nrmatricol
+            order by medie desc
+        )
 
 --15. Studentul de pe pozitia 5 in ordinea mediilor, din fiecare sectie.
+select denumires, nume, media
+    from sectii inner join(
+        select s1.nrmatricol, s1.cods, s1.nume, AVG(nota) as media
+            from studenti as s1 inner join rezultate as r1
+            on s1.nrmatricol = r1.nrmatricol
+            group by s1.nrmatricol, s1.cods, s1.nume
+    ) as rez1
+    on rez1.cods = sectii.cods
+    where
+        media = (
+            select top 1 media
+                from(
+                    select top 5 AVG(nota) as media
+                        from studenti as s2 inner join rezultate as r2
+                        on s2.nrmatricol = r2.nrmatricol
+                        where s2.cods = rez1.cods
+                        group by s2.nrmatricol, s2.cods, s2.nume
+                        order by media desc
+                ) as rez2
+                order by media asc
+        )
 
 --16. Sectiile la care se predau cele mai multe discipline si numarul acestora (al disciplinelor predate).
+select top 1 with ties sectii.denumires, COUNT(*) as numarDiscipline
+    from sectii inner join planinv
+        on sectii.cods = planinv.cods
+    group by sectii.cods, sectii.denumires
+    order by numarDiscipline desc
 
 --17. Pentru fiecare student – numarul total de note, numarul de note >=5, numarul de note <5 (optional, vezi
 --    CASE – SQL-Server documentation).
+select *,
+    noteTotale =
+        case when 1 = 1 then
+            (select COUNT(*)
+                from rezultate as r1
+                where r1.nrmatricol = studenti.nrmatricol)
+        end,
+    noteDeTrecere =
+        case when 1 = 1 then
+            (select COUNT(*)
+                from rezultate as r2
+                where
+                    r2.nrmatricol = studenti.nrmatricol
+                    and
+                    r2.nota >= 5)
+        end,
+    noteSubCinci =
+        case when 1 = 1 then
+            (select COUNT(*)
+                from rezultate as r3
+                where
+                    r3.nrmatricol = studenti.nrmatricol
+                    and
+                    r3.nota < 5)
+        end
+    from studenti
 
 --18. Stiind ca din fiecare sectie 40% din studenti primesc bursa, sa se afiseze studentii care
 --    primesc bursa din fiecare sectie (un student primeste bursa daca toate notele sale sunt de trecere).
+select s1.nume, denumires
+    from studenti as s1 inner join sectii
+    on sectii.cods = s1.cods
+    where s1.nrmatricol in(
+        select rez1.nrmatricol
+            from(
+                select top 40 percent s3.nrmatricol, AVG(nota) as media
+                    from rezultate inner join studenti as s3
+                    on s3.nrmatricol = rezultate.nrmatricol
+                    where
+                        s3.nrmatricol = s1.nrmatricol
+                        and
+                        5 < all(
+                            select nota 
+                                from rezultate
+                                where rezultate.nrmatricol = s1.nrmatricol
+                        )
+                    group by s3.nrmatricol
+                    order by media desc
+            ) as rez1
+    )
 
 --19. Studentii care nu au cea mai mica nota din grupa din care fac parte.
+select s1.*, media
+    from studenti as s1 inner join(
+        select nrmatricol, AVG(nota) as media
+            from rezultate
+            group by nrmatricol
+    ) as rez1
+    on s1.nrmatricol = rez1.nrmatricol
+    where media <= all(
+        select AVG(nota)
+            from rezultate inner join studenti as s2
+            on rezultate.nrmatricol = s2.nrmatricol
+            where s2.grupa = s1.grupa
+            group by s2.nrmatricol
+    )
 
 --20. Pentru fiecare sectie se afiseaza numarul de studenti. Pentru sectiile in care nu este nici un student
 --    se afiseaza 0.
+select denumires, numarStudenti = case when studenti.cods is null then 0 else COUNT(*) end
+    from sectii left join studenti
+    on sectii.cods = studenti.cods
+    group by denumires, studenti.cods
+
