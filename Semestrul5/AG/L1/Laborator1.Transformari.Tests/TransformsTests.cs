cf01c867510fd438148790c4468462cc367eeb18
Andrei Fangli <andrei_fangli@hotmail.com>
2013-10-04 18:35:50 +0300

    Adaug AG Lab1 implementare

---
+++ Semestrul5/AG/L1/Laborator1.Transformari.Tests/TransformsTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace AlgoritmicaGrafelor.Laborator1.Transformari.Tests
+{
+    [TestClass]
+    public class TransformsTests
+    {
+        [TestClass]
+        public class ToSuccessorListTests
+        {
+            [TestMethod]
+            public void TestWhenAdjacencyMatrixIsEmpty()
+            {
+                var SuccessorList = Transforms.ToSuccessorList(new bool[,] { });
+
+                Assert.AreEqual(0, SuccessorList.Count);
+            }
+
+            [TestMethod]
+            public void TestWhenAdjacencyMatrixContainsOnlyFalse()
+            {
+                var SuccessorList = Transforms.ToSuccessorList(new bool[,] { { false, false }, { false, false } });
+
+                Assert.AreEqual(2, SuccessorList.Count);
+                Assert.AreEqual(0, SuccessorList[0].Count);
+                Assert.AreEqual(0, SuccessorList[1].Count);
+            }
+
+            [TestMethod]
+            public void TestWhenAdjacencyMatrixContainsOneTrueOnFirstPosition()
+            {
+                var SuccessorList = Transforms.ToSuccessorList(new bool[,] { { true, false }, { false, false } });
+
+                Assert.AreEqual(2, SuccessorList.Count);
+                Assert.AreEqual(1, SuccessorList[0].Count);
+                Assert.AreEqual(0, SuccessorList[1].Count);
+                Assert.AreEqual(0, SuccessorList[0][0]);
+            }
+
+            [TestMethod]
+            public void TestWhenAdjacencyMatrixContainsContainsOnlyTrue()
+            {
+                var SuccessorList = Transforms.ToSuccessorList(new bool[,] { { true, true }, { true, true } });
+
+                Assert.AreEqual(2, SuccessorList.Count);
+                Assert.AreEqual(2, SuccessorList[0].Count);
+                Assert.AreEqual(0, SuccessorList[0][0]);
+                Assert.AreEqual(1, SuccessorList[0][1]);
+                Assert.AreEqual(2, SuccessorList[1].Count);
+                Assert.AreEqual(0, SuccessorList[1][0]);
+                Assert.AreEqual(1, SuccessorList[1][1]);
+            }
+
+            [TestMethod]
+            public void TestWhenAdjacencyMatrixHasCircularReference()
+            {
+                var SuccessorList = Transforms.ToSuccessorList(new bool[,] { { false, true }, { true, false } });
+
+                Assert.AreEqual(2, SuccessorList.Count);
+                Assert.AreEqual(1, SuccessorList[0].Count);
+                Assert.AreEqual(1, SuccessorList[0][0]);
+                Assert.AreEqual(1, SuccessorList[1].Count);
+                Assert.AreEqual(0, SuccessorList[1][0]);
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+            public void TestWhenAdjacencyMatrixIsNull()
+            {
+                Transforms.ToSuccessorList(null);
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentException))]
+            public void TestWhenAdjacencyMatrixIsNotSquareMatrix()
+            {
+                Transforms.ToSuccessorList(new bool[,] { { false }, { true } });
+            }
+        }
+
+        [TestClass]
+        public class ToAdjacencyMatrixTests
+        {
+            [TestMethod]
+            public void TestWhenSuccessorsListIsEmpty()
+            {
+                var adjacencyMatrix = Transforms.ToAdjacencyMatrix(new List<IList<int>>());
+
+                Assert.AreEqual(0, adjacencyMatrix.Length);
+            }
+
+            [TestMethod]
+            public void TestWhenSuccessorsListHas4Nulls()
+            {
+                var adjacencyMatrix = Transforms.ToAdjacencyMatrix(new List<IList<int>>() { null, null, null, null });
+
+                Assert.AreEqual(16, adjacencyMatrix.Length);
+                for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 4; j++)
+                        Assert.IsFalse(adjacencyMatrix[i, j]);
+            }
+        }
+    }
+}
---
+++ Semestrul5/AG/L1/Laborator1.Transformari/Transforms.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoritmicaGrafelor.Laborator1.Transformari
+{
+    public static class Transforms
+    {
+        /// <exception cref="System.ArgumentNullException">Thrown when adjancency matrix is null.</exception>
+        /// <exception cref="System.ArgumentException">Thrown when the adjacency matrix is not a square matrix.</exception>
+        static public IList<IList<int>> ToSuccessorList(bool[,] adjacencyMatrix)
+        {
+            if (adjacencyMatrix != null)
+            {
+                int numberOfPeaks = (int)Math.Sqrt(adjacencyMatrix.Length);
+
+                if (Math.Pow(numberOfPeaks, 2) == adjacencyMatrix.Length)
+                {
+                    IList<IList<int>> SuccessorsList = new List<IList<int>>(numberOfPeaks);
+
+                    for (int i = 0; i < numberOfPeaks; i++)
+                    {
+                        IList<int> SuccessorPeaks = new List<int>();
+
+                        for (int j = 0; j < numberOfPeaks; j++)
+                            if (adjacencyMatrix[i, j])
+                                SuccessorPeaks.Add(j);
+
+                        SuccessorsList.Add(SuccessorPeaks);
+                    }
+
+                    return SuccessorsList;
+                }
+                else
+                    throw new ArgumentException("The adjacencyMatrix is not a square matrix!");
+            }
+            else
+                throw new ArgumentNullException("adjacencyMatrix");
+        }
+
+        /// <exception cref="System.ArgumentNullException">Thrown when successorsList is null.</exception>
+        /// <exception cref="System.ArgumentException">Thrown when the successorsList contains a peak not present in the graph.</exception>
+        static public bool[,] ToAdjacencyMatrix(IList<IList<int>> successorsList)
+        {
+            if (successorsList != null)
+            {
+                bool[,] adjacencyMatrix = new bool[successorsList.Count, successorsList.Count];
+
+                for (int i = 0; i < successorsList.Count; i++)
+                    if (successorsList[i] != null)
+                        foreach (int successor in successorsList[i])
+                            if (0 <= successor && successor <= successorsList.Count)
+                                adjacencyMatrix[i, successor] = true;
+                            else
+                                throw new ArgumentException("The successor " + successor + " is not a peak in the graph!");
+                return adjacencyMatrix;
+            }
+            else
+                throw new ArgumentNullException("successorList");
+        }
+    }
+}
