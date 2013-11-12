f56a8da9ccc736ac9d5404ae435e49bbdb64b6f9
Andrei Fangli <andrei_fangli@hotmail.com>
2013-11-12 17:42:06 +0200

    Adaug documentatie LFTC L2.1

--- Semestrul5/LFTC/L2/Partea1/Views/MainWindow.xaml.cs
+++ Semestrul5/LFTC/L2/Partea1/Views/MainWindow.xaml.cs
@@ -7,25 +7,12 @@ namespace Partea1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    internal partial class MainWindow
-        : Window, INotifyPropertyChanged
+    internal sealed partial class MainWindow
+        : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void _OnPropertyChanged(string propertyName)
-        {
-            if (propertyName != null)
-            {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-            else
-                throw new ArgumentNullException("propertyName");
         }
     }
 }
