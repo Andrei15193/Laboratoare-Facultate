cf4d847de13370ddee7bcbbae719e260b73949c4
Andrei Fangli <andrei_fangli@hotmail.com>
2013-11-29 00:14:48 +0200

    Adaug AG Lab4

---
+++ Semestrul5/AG/L4/Laborator4.Multimi.Tests/MultimiInteriorStabileTests.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AlgoritmicaGrafelor.Laborator4.Multimi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoritmicaGrafelor.Laborator4.Tests
+{
+    [TestClass]
+    public class MultimiInteriorStabileTests
+    {
+        static private readonly IReadOnlyList<IReadOnlyList<int>> _onePeekGraph = new[]
+            {
+                new List<int>{ }
+            };
+        static private readonly IReadOnlyList<IReadOnlyList<int>> _twoPeekGraph = new[]
+            {
+                new List<int>{ 1 },
+                new List<int>{ }
+            };
+        static private readonly IReadOnlyList<IReadOnlyList<int>> _fourPeekGraph = new[]
+            {
+                new List<int>{ 1, 2 },
+                new List<int>{ 3 },
+                new List<int>{ 3 },
+                new List<int>{ }
+            };
+        static private readonly IReadOnlyList<IReadOnlyList<int>> _fivePeekGraphWithACycle = new[]
+            {
+                new List<int>{ 1 },
+                new List<int>{ 2, 4 },
+                new List<int>{ 3 },
+                new List<int>{ 1 },
+                new List<int>{ }
+            };
+
+        [TestMethod]
+        public void TestForOnePeekGraph()
+        {
+            IReadOnlyList<IReadOnlyList<int>> multimiInteriorStabile = MultimiInteriorStabile.Toate(_onePeekGraph);
+
+            Assert.AreEqual(2, multimiInteriorStabile.Count);
+            Assert.IsTrue(multimiInteriorStabile[0].Count == 1 || multimiInteriorStabile[1].Count == 0);
+            if (multimiInteriorStabile[0].Count == 1)
+            {
+                Assert.AreEqual(0, multimiInteriorStabile[0][0]);
+                Assert.AreEqual(0, multimiInteriorStabile[1].Count);
+            }
+            else
+            {
+                Assert.AreEqual(0, multimiInteriorStabile[1][0]);
+                Assert.AreEqual(0, multimiInteriorStabile[0].Count);
+            }
+        }
+
+        [TestMethod]
+        public void TestForTwoPeekGraph()
+        {
+            IReadOnlyList<IReadOnlyList<int>> multimiInteriorStabile = MultimiInteriorStabile.Toate(_twoPeekGraph)
+                                                                                             .OrderBy(multime => multime.Count)
+                                                                                             .ToList();
+
+            Assert.AreEqual(3, multimiInteriorStabile.Count);
+            Assert.AreEqual(0, multimiInteriorStabile.Where(multime => multime.Count == 1)
+                                                     .Select(multime => multime.First())
+                                                     .Except(new[] { 0, 1 })
+                                                     .Count());
+        }
+
+        [TestMethod]
+        public void TestForFourPeekGraph()
+        {
+            IReadOnlyList<IReadOnlyList<int>> multimiInteriorStabile = MultimiInteriorStabile.Toate(_fourPeekGraph)
+                                                                                             .OrderBy(multime => multime.Count)
+                                                                                             .ToList();
+
+            Assert.AreEqual(7, multimiInteriorStabile.Count);
+            Assert.AreEqual(0, multimiInteriorStabile.Where(multime => multime.Count == 1)
+                                                     .Select(multime => multime.First())
+                                                     .Except(new[] { 0, 1, 2, 3 })
+                                                     .Count());
+
+            var misCuDouaElement = multimiInteriorStabile.Where(multime => multime.Count == 2)
+                                                         .Select(multime => multime.OrderBy(element => element))
+                                                         .OrderBy(multime => string.Join(", ", multime.Select(element => element.ToString())))
+                                                         .ToList();
+            Assert.AreEqual(2, misCuDouaElement.Count);
+            Assert.IsTrue(new[] { 0, 3 }.SequenceEqual(misCuDouaElement[0]));
+            Assert.IsTrue(new[] { 1, 2 }.SequenceEqual(misCuDouaElement[1]));
+        }
+
+        [TestMethod]
+        public void TestForFivePeekGraphWithACycle()
+        {
+            IReadOnlyList<IReadOnlyList<int>> multimiInteriorStabile = MultimiInteriorStabile.Toate(_fivePeekGraphWithACycle)
+                                                                                             .OrderBy(multime => multime.Count)
+                                                                                             .ToList();
+
+            Assert.AreEqual(13, multimiInteriorStabile.Count);
+            Assert.AreEqual(0, multimiInteriorStabile.Where(multime => multime.Count == 1)
+                                                     .Select(multime => multime.First())
+                                                     .Except(new[] { 0, 1, 2, 3, 4 })
+                                                     .Count());
+
+            var misCuDouaElement = multimiInteriorStabile.Where(multime => multime.Count == 2)
+                                                         .Select(multime => multime.OrderBy(element => element))
+                                                         .OrderBy(multime => string.Join(", ", multime.Select(element => element.ToString())))
+                                                         .ToList();
+            Assert.AreEqual(5, misCuDouaElement.Count);
+            Assert.IsTrue(new[] { 0, 2 }.SequenceEqual(misCuDouaElement[0]));
+            Assert.IsTrue(new[] { 0, 3 }.SequenceEqual(misCuDouaElement[1]));
+            Assert.IsTrue(new[] { 0, 4 }.SequenceEqual(misCuDouaElement[2]));
+            Assert.IsTrue(new[] { 2, 4 }.SequenceEqual(misCuDouaElement[3]));
+            Assert.IsTrue(new[] { 3, 4 }.SequenceEqual(misCuDouaElement[4]));
+            var misCuTreiElement = multimiInteriorStabile.Where(multime => multime.Count == 3)
+                                                         .Select(multime => multime.OrderBy(element => element))
+                                                         .OrderBy(multime => string.Join(", ", multime.Select(element => element.ToString())))
+                                                         .ToList();
+
+            Assert.AreEqual(2, misCuTreiElement.Count);
+            Assert.IsTrue(new[] { 0, 2, 4 }.SequenceEqual(misCuTreiElement[0]));
+            Assert.IsTrue(new[] { 0, 3, 4 }.SequenceEqual(misCuTreiElement[1]));
+        }
+    }
+}
---
+++ Semestrul5/AG/L4/Laborator4.Multimi/MultimiInteriorStabile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoritmicaGrafelor.Laborator4.Multimi
+{
+    public static class MultimiInteriorStabile
+    {
+        static public IReadOnlyList<IReadOnlyList<int>> Toate(IReadOnlyList<IReadOnlyList<int>> listaSuccesorilor)
+        {
+            if (listaSuccesorilor != null)
+                return _DeterminaSubmultimi(_Varfuri(listaSuccesorilor)).Where(submultime => _EsteInteriorStabila(submultime, listaSuccesorilor))
+                                                                        .ToList();
+            else
+                throw new ArgumentNullException("listaSuccesorilor");
+        }
+
+        static private IReadOnlyList<IReadOnlyList<int>> _DeterminaSubmultimi(IReadOnlyCollection<int> multime)
+        {
+            if (multime != null)
+            {
+                IEqualityComparer<IEnumerable<int>> listElementsEqualityComparer = new ListElementsEqualityComparer<int>();
+                HashSet<IReadOnlyList<int>> submultimi = new HashSet<IReadOnlyList<int>>(listElementsEqualityComparer);
+                HashSet<IReadOnlyList<int>> multimiRamase = new HashSet<IReadOnlyList<int>>(listElementsEqualityComparer) { multime.ToList() };
+
+                do
+                {
+                    IReadOnlyList<int> multimeRamasa = multimiRamase.First();
+                    multimiRamase.Remove(multimeRamasa);
+                    submultimi.Add(multimeRamasa);
+
+                    foreach (int element in multimeRamasa)
+                        multimiRamase.Add(multimeRamasa.Except(new[] { element }).ToList());
+                } while (multimiRamase.Count > 0);
+
+                return submultimi.ToList();
+            }
+            else
+                throw new ArgumentNullException("multime");
+        }
+
+        static private IReadOnlyList<int> _Varfuri(IReadOnlyList<IReadOnlyList<int>> listaSuccesorilor)
+        {
+            List<int> varfuri = new List<int>();
+
+            for (int varf = 0; varf < listaSuccesorilor.Count; varf++)
+                varfuri.Add(varf);
+
+            return varfuri;
+        }
+
+        static private bool _EsteInteriorStabila(IReadOnlyList<int> multimeVarfuri, IReadOnlyList<IReadOnlyList<int>> listaSuccesorilor)
+        {
+            int varf = 0;
+
+            while (varf < listaSuccesorilor.Count
+                   && (!multimeVarfuri.Contains(varf)
+                       || listaSuccesorilor[varf].Intersect(multimeVarfuri).Count() == 0))
+                varf++;
+
+            return (varf == listaSuccesorilor.Count);
+        }
+
+        private sealed class ListElementsEqualityComparer<T>
+            : IEqualityComparer<IEnumerable<T>>
+        {
+            public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
+            {
+                return x.SequenceEqual(y);
+            }
+
+            public int GetHashCode(IEnumerable<T> obj)
+            {
+                return obj.Aggregate(0, (currentHashCode, element) => (currentHashCode ^ element.GetHashCode()));
+            }
+        }
+
+    }
+}
