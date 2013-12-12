bba4fddc57759745acfc14175ee9686cc32843fa
Andrei Fangli <andrei_fangli@hotmail.com>
2013-12-12 22:48:15 +0200

    Adaug L5 AG (Prim)

---
+++ Semestrul5/AG/L5/Subarbori.Tests/PrimTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AlgoritmicaGrafelor.Laborator5.Subarbori.Tests
+{
+	[TestClass]
+	public class PrimTests
+	{
+		static private readonly IReadOnlyCollection<Tuple<int, int, double>> _onePeekGraph =
+			new Tuple<int, int, double>[0];
+		static private readonly IReadOnlyCollection<Tuple<int, int, double>> _twoPeekGraph =
+			new[] { Tuple.Create(0, 1, 1d) };
+		static private readonly IReadOnlyCollection<Tuple<int, int, double>> _threePeekGraph =
+			new[] { Tuple.Create(0, 1, 1d),
+					Tuple.Create(0, 2, 2d),
+					Tuple.Create(1, 2, 1d) };
+		static private readonly IReadOnlyCollection<Tuple<int, int, double>> _fourPeekGraph =
+			new[] { Tuple.Create(1, 2, 4d),
+					Tuple.Create(0, 2, 2d),
+					Tuple.Create(0, 3, 3d),
+					Tuple.Create(3, 2, 1d) };
+		[TestMethod]
+		public void TestForOnePeekGraph()
+		{
+			IReadOnlyCollection<Tuple<int, int, double>> result = SubarborePartial.Prim(_onePeekGraph);
+			Assert.IsNotNull(result);
+			Assert.AreEqual(0, result.Count);
+		}
+		[TestMethod]
+		public void TestForTwoPeekGraph()
+		{
+			IReadOnlyCollection<Tuple<int, int, double>> result = SubarborePartial.Prim(_twoPeekGraph);
+			Assert.IsNotNull(result);
+			Assert.AreEqual(1, result.Count);
+			Assert.IsTrue(result.SequenceEqual(_twoPeekGraph));
+		}
+		[TestMethod]
+		public void TestForThreePeekGraph()
+		{
+			IReadOnlyCollection<Tuple<int, int, double>> result = SubarborePartial.Prim(_threePeekGraph);
+			Assert.IsNotNull(result);
+			Assert.AreEqual(2, result.Count);
+			Assert.IsTrue(result.OrderBy(tuple => tuple.Item2)
+								.OrderBy(tuple => tuple.Item1)
+								.SequenceEqual(new[]
+								{
+									Tuple.Create(0, 1, 1d),
+									Tuple.Create(1, 2, 1d)
+								}));
+		}
+		[TestMethod]
+		public void TestForFourPeekGraph()
+		{
+			IReadOnlyCollection<Tuple<int, int, double>> result = SubarborePartial.Prim(_fourPeekGraph);
+			Assert.IsNotNull(result);
+			Assert.AreEqual(3, result.Count);
+			Assert.IsTrue(result.OrderBy(tuple => tuple.Item2)
+								.OrderBy(tuple => tuple.Item1)
+								.SequenceEqual(new[]
+								{
+									Tuple.Create(0, 2, 2d),
+									Tuple.Create(1, 2, 4d),
+									Tuple.Create(3, 2, 1d) 
+								}));
+		}
+	}
+}
---
+++ Semestrul5/AG/L5/Subarbori/SubarborePartial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AlgoritmicaGrafelor.Laborator5.Subarbori
+{
+	public static class SubarborePartial
+	{
+		static public IReadOnlyCollection<Tuple<int, int, double>> Prim(IReadOnlyCollection<Tuple<int, int, double>> multimeaMuchiilor)
+		{
+			if (multimeaMuchiilor == null)
+				throw new ArgumentNullException("muchii");
+			if (multimeaMuchiilor.Count == 0)
+				return new Tuple<int, int, double>[0];
+			IReadOnlyCollection<int> varfuri = multimeaMuchiilor.SelectMany(muchie => new[] { muchie.Item1, muchie.Item2 })
+														  .Distinct()
+														  .ToList();
+			ISet<int> varfuriVizitate = new HashSet<int> { varfuri.First() };
+			ISet<Tuple<int, int, double>> muchiiVizitate = new HashSet<Tuple<int, int, double>>();
+			while (varfuriVizitate.Count != varfuri.Count)
+			{
+				IList<Tuple<int, int, double>> muchii = new List<Tuple<int, int, double>>();
+				foreach (int varf in varfuriVizitate)
+				{
+					Tuple<int, int, double> muchieAdiacentaDeValoareMinima =
+						multimeaMuchiilor.Where(m => (m.Item1 == varf || m.Item2 == varf)
+													  && varfuriVizitate.Contains(m.Item1) != varfuriVizitate.Contains(m.Item2))
+										 .OrderBy(m => m.Item3)
+										 .FirstOrDefault();
+					if (muchieAdiacentaDeValoareMinima != null)
+						muchii.Add(muchieAdiacentaDeValoareMinima);
+				}
+				Tuple<int, int, double> muchie = muchii.OrderBy(m => m.Item3).First();
+				varfuriVizitate.Add(muchie.Item1);
+				varfuriVizitate.Add(muchie.Item2);
+				muchiiVizitate.Add(muchie);
+			}
+			return muchiiVizitate.ToList();
+		}
+	}
+}
