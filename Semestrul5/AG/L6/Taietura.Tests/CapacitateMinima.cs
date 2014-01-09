995dd0e092bc9100d944b83ab36d8330d476a4a3
Andrei Fangli <andrei_fangli@hotmail.com>
2014-01-09 22:33:35 +0200

    Adaug AG Lab6

---
+++ Semestrul5/AG/L6/Taietura.Tests/CapacitateMinima.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AlgoritmicaGrafelor.Laborator6.Taietura.Tests
+{
+	[TestClass]
+	public class CapacitateMinima
+	{
+		IReadOnlyList<IReadOnlyCollection<Tuple<int, double>>> _grafCuDouaVarfuri = new[]
+		{
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(1, 1d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)new Tuple<int, double>[0]
+		};
+		IReadOnlyList<IReadOnlyCollection<Tuple<int, double>>> _grafCuPatruVarfuriSiDouaDrumuriDeLaSursaLadestinatie = new[]
+		{
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(1, 1d), Tuple.Create(2, 2d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(3, 3d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(3, 3d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)new Tuple<int, double>[0]
+		};
+		IReadOnlyList<IReadOnlyCollection<Tuple<int, double>>> _grafCuCinciVarfuriSiCircuit = new[]
+		{
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(1, 1d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(2, 2d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(3, 3d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)(new []{ Tuple.Create(1, 1d), Tuple.Create(4, 4d) }),
+			(IReadOnlyCollection<Tuple<int, double>>)new Tuple<int, double>[0]
+		};
+		[TestMethod]
+		public void TestGrafCuDouaVarfuri()
+		{
+			Assert.AreEqual(1d, Taietura.CapacitateMinima(_grafCuDouaVarfuri));
+		}
+		[TestMethod]
+		public void TestGrafCuPatruVarfuriSiDouaDrumuriDeLaSursaLadestinatie()
+		{
+			Assert.AreEqual(3d, Taietura.CapacitateMinima(_grafCuPatruVarfuriSiDouaDrumuriDeLaSursaLadestinatie));
+		}
+		[TestMethod]
+		public void TestGrafCuCinciVarfuriSiCircuit()
+		{
+			Assert.AreEqual(1d, Taietura.CapacitateMinima(_grafCuCinciVarfuriSiCircuit));
+		}
+	}
+}
---
+++ Semestrul5/AG/L6/Taietura/Taietura.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AlgoritmicaGrafelor.Laborator6.Taietura
+{
+	public static class Taietura
+	{
+		public static double CapacitateMinima(IReadOnlyList<IReadOnlyCollection<Tuple<int, double>>> graf)
+		{
+			if (graf == null)
+				throw new ArgumentNullException("graf");
+			IList<IList<Tuple<int, double>>> grafulEcarturilor = new List<IList<Tuple<int, double>>>(graf.Select(listaSuccesori => (IList<Tuple<int, double>>)listaSuccesori.ToList()));
+			IReadOnlyCollection<int> varfuriFaraSuccesori = _DeterminaVarfuriFaraSuccesori(grafulEcarturilor);
+			IReadOnlyCollection<int> varfuriFaraPredecesori = _DeterminaVarfuriFaraPredecesori(grafulEcarturilor);
+			if (varfuriFaraSuccesori.Count != 1 || varfuriFaraPredecesori.Count != 1 || !_EConex(grafulEcarturilor))
+				return double.NegativeInfinity;
+			int sursa = varfuriFaraPredecesori.First();
+			int destinatie = varfuriFaraSuccesori.First();
+			double flux = 0;
+			IReadOnlyList<Tuple<int, double>> drum = _DeterminaDrum(grafulEcarturilor, sursa, destinatie);
+			do
+			{
+				double min = drum.Min(varf => varf.Item2);
+				for (int indexVarf = 0, varfPrecedent = sursa; indexVarf < drum.Count; indexVarf++)
+				{
+					Tuple<int, double> varf = grafulEcarturilor[varfPrecedent].First(v => (v == drum[indexVarf]));
+					double capacitateActuala = (varf.Item2 - min);
+					grafulEcarturilor[varfPrecedent].Remove(varf);
+					if (capacitateActuala > 0)
+						grafulEcarturilor[varfPrecedent].Add(Tuple.Create(varf.Item1, capacitateActuala));
+					varfPrecedent = drum[indexVarf].Item1;
+				}
+				flux += min;
+				drum = _DeterminaDrum(grafulEcarturilor, sursa, destinatie);
+			} while (drum.Count > 0);
+			return flux;
+		}
+		private static IReadOnlyCollection<int> _DeterminaVarfuriFaraPredecesori(IList<IList<Tuple<int, double>>> graf)
+		{
+			ISet<int> multime = new HashSet<int>();
+			for (int i = 0; i < graf.Count; i++)
+				multime.Add(i);
+			for (int i = 0; i < graf.Count; i++)
+				multime = new HashSet<int>(multime.Except(graf[i].Select(succesor => succesor.Item1)));
+			return multime.ToList();
+		}
+		private static IReadOnlyCollection<int> _DeterminaVarfuriFaraSuccesori(IList<IList<Tuple<int, double>>> graf)
+		{
+			ISet<int> multime = new HashSet<int>();
+			for (int i = 0; i < graf.Count; i++)
+				if (graf[i].Count == 0)
+					multime.Add(i);
+			return multime.ToList();
+		}
+		private static bool _EConex(IList<IList<Tuple<int, double>>> graf)
+		{
+			if (graf.Count == 0)
+				return true;
+			ISet<int> multime = new HashSet<int>(graf[0].Select(succesor => succesor.Item1));
+			for (int i = 1; i < graf.Count; i++)
+				multime = new HashSet<int>(multime.Union(graf[1].Select(succesor => succesor.Item1)));
+			return (multime.Count(varf => (varf >= graf.Count)) == 0);
+		}
+		private static IReadOnlyList<Tuple<int, double>> _DeterminaDrum(IList<IList<Tuple<int, double>>> graf, int sursa, int destinatie)
+		{
+			List<Tuple<int, double>> drum = new List<Tuple<int, double>>();
+			int candidat = sursa;
+			while (candidat != destinatie &&
+				   graf[candidat].Except(drum).FirstOrDefault() != null)
+			{
+				Tuple<int, double> ultimulVarf = graf[candidat].Except(drum).First();
+				drum.Add(ultimulVarf);
+				candidat = ultimulVarf.Item1;
+			}
+			if (drum.Count > 0 && drum.Last().Item1 != destinatie)
+				drum.Clear();
+			return drum;
+		}
+	}
+}
