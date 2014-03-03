edc2648d85beb1b0d66e57a7ec0e504c891f6459
Andrei Fangli <andrei_fangli@hotmail.com>
2014-03-03 17:03:53 +0200

    Adaugat comentarii

--- Semestrul6/MPP/L1/P8/Program.cs
+++ Semestrul6/MPP/L1/P8/Program.cs
@@ -7,46 +7,61 @@ namespace P8
 	{
 		static Program()
 		{
-			_numberCorespondence.Add(0, "zero");
-			_numberCorespondence.Add(1, "unu");
-			_numberCorespondence.Add(2, "doi");
-			_numberCorespondence.Add(3, "trei");
-			_numberCorespondence.Add(4, "patru");
-			_numberCorespondence.Add(5, "cinci");
-			_numberCorespondence.Add(6, "sase");
-			_numberCorespondence.Add(7, "sapte");
-			_numberCorespondence.Add(8, "opt");
-			_numberCorespondence.Add(9, "noua");
-			_numberCorespondence.Add(10, "zece");
+			_numere.Add(0, "zero");
+			_numere.Add(1, "unu");
+			_numere.Add(2, "doi");
+			_numere.Add(3, "trei");
+			_numere.Add(4, "patru");
+			_numere.Add(5, "cinci");
+			_numere.Add(6, "sase");
+			_numere.Add(7, "sapte");
+			_numere.Add(8, "opt");
+			_numere.Add(9, "noua");
+			_numere.Add(10, "zece");
 		}
 
 		internal static void Main()
 		{
+			int numar = _CitesteIntreg("Introduceti un numar intre -1000 si 1000 inclusiv: ", -1000, 1000);
+			string numarScrisInCuvinte = _ObtineNumarScrisInCuvinte(numar);
+			Console.WriteLine("{0} = {1}, care are {2} litere", numar, numarScrisInCuvinte, numarScrisInCuvinte.Replace(" ", string.Empty).Length);
+		}
+
+		/// <summary>
+		/// Transforma numarul dat in reprezentarea lui scrisa in cuvinte.
+		/// </summary>
+		/// <param name="numar">Numar de transformat.</param>
+		/// <returns>Returneaza numarul in reprezentarea sa scrisa in cuvinte.</returns>
+		/// <exception cref="System.ArgumentException">Cand numarul intreg este mai mic decat -1000 sau mai mare decat 1000.</exception>
+		private static string _ObtineNumarScrisInCuvinte(int numar)
+		{
+			if (numar < -1000 || 1000 < numar)
+				throw new ArgumentException("Numarul trebuie sa fie mai mare egal cu -1000 si mai mic egal cu 1000!", "numar");
+
 			bool writeZero = true;
-			int number = _ReadInteger("Introduceti un numar intre -1000 si 1000 inclusiv: ", -1000, 1000), numberCopy = number;
-			StringBuilder numberStringBuilder = new StringBuilder();
+			StringBuilder numarInCuvinteStringBuilder = new StringBuilder();
 
-			if (number < 0)
-				numberStringBuilder.Append("minus ");
+			if (numar < 0)
+				numarInCuvinteStringBuilder.Append("minus ");
 
-			number = Math.Abs(number);
-			if (number == 1000)
+			numar = Math.Abs(numar);
+			if (numar == 1000)
 			{
-				numberStringBuilder.Append("o mie ");
-				number %= 1000;
+				numarInCuvinteStringBuilder.Append("o mie ");
+				numar %= 1000;
 				writeZero = false;
 			}
 
-			switch (number / 100)
+			switch (numar / 100)
 			{
 				case 0:
 					break;
 				case 1:
-					numberStringBuilder.Append("o suta ");
+					numarInCuvinteStringBuilder.Append("o suta ");
 					writeZero = false;
 					break;
 				case 2:
-					numberStringBuilder.Append("doua sute ");
+					numarInCuvinteStringBuilder.Append("doua sute ");
 					writeZero = false;
 					break;
 				case 3:
@@ -56,54 +71,58 @@ namespace P8
 				case 7:
 				case 8:
 				case 9:
-					numberStringBuilder.Append(_numberCorespondence[number / 100]).Append(" sute ");
+					numarInCuvinteStringBuilder.Append(_numere[numar / 100]).Append(" sute ");
 					writeZero = false;
 					break;
 			}
-			number %= 100;
+			numar %= 100;
 
-			if (number <= 10)
+			if (numar <= 10)
 			{
-				if (number != 0 || writeZero)
-					numberStringBuilder.Append(_numberCorespondence[number]);
+				if (numar != 0 || writeZero)
+					numarInCuvinteStringBuilder.Append(_numere[numar]);
 			}
 			else
-				if (number == 11)
-					numberStringBuilder.Append("unsprezece");
+				if (numar == 11)
+					numarInCuvinteStringBuilder.Append("unsprezece");
 				else
-					if (number < 20)
-						numberStringBuilder.Append(_numberCorespondence[number % 10]).Append("sprezece");
+					if (numar < 20)
+						numarInCuvinteStringBuilder.Append(_numere[numar % 10]).Append("sprezece");
 					else
-						if (number < 30)
+						if (numar < 30)
 						{
-							numberStringBuilder.Append("douazeci");
-							if (number % 10 != 0)
-								numberStringBuilder.Append(" si ").Append(_numberCorespondence[number % 10]);
+							numarInCuvinteStringBuilder.Append("douazeci");
+							if (numar % 10 != 0)
+								numarInCuvinteStringBuilder.Append(" si ").Append(_numere[numar % 10]);
 						}
 						else
 						{
-							numberStringBuilder.Append(_numberCorespondence[number / 10] + "zeci");
-							if (number % 10 != 0)
-								numberStringBuilder.Append(" si ").Append(_numberCorespondence[number % 10]);
+							numarInCuvinteStringBuilder.Append(_numere[numar / 10] + "zeci");
+							if (numar % 10 != 0)
+								numarInCuvinteStringBuilder.Append(" si ").Append(_numere[numar % 10]);
 						}
 
-			string numberAsNameString = numberStringBuilder.ToString().Trim();
-
-			Console.WriteLine("{0} = {1}, care are {2} litere", numberCopy, numberAsNameString, numberAsNameString.Replace(" ", string.Empty).Length);
+			return numarInCuvinteStringBuilder.ToString().Trim();
 		}
-
-		private static int _ReadInteger(string message, int minValue, int maxValue)
+		/// <summary>
+		/// Citeste un intreg de la intrarea standard aflta intre valorile specificate (inclusiv)
+		/// </summary>
+		/// <param name="mesaj">Mesajul care este afisat inainte de citirea numarului intreg.</param>
+		/// <param name="valoareMinima">Valoarea minima inclusiva care poate fi citita.</param>
+		/// <param name="valoareMaxima">Valoarea maxima inclusiva care poate fi citita.</param>
+		/// <returns>Un numar intreg citit de la intrarea standard aflta intre valoareMinima si valoareMaxima inclusiv.</returns>
+		private static int _CitesteIntreg(string mesaj, int valoareMinima, int valoareMaxima)
 		{
-			int integer;
+			int numar;
 
 			do
 				do
-					Console.Write(message);
-				while (!int.TryParse(Console.ReadLine(), out integer));
-			while (integer < minValue || maxValue < integer);
+					Console.Write(mesaj ?? string.Empty);
+				while (!int.TryParse(Console.ReadLine(), out numar));
+			while (numar < valoareMinima || valoareMaxima < numar);
 
-			return integer;
+			return numar;
 		}
-		private static readonly IDictionary<int, string> _numberCorespondence = new Dictionary<int, string>();
+		private static readonly IDictionary<int, string> _numere = new Dictionary<int, string>();
 	}
 }
