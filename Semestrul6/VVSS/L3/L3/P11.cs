cd205f9d5b2cd1f0b967c230a46760c2b4e16813
Andrei Fangli <andrei_fangli@hotmail.com>
2014-04-13 23:40:12 +0300

    Adaug Lab3.11 VVSS

---
+++ Semestrul6/VVSS/L3/L3/P11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace L3
+{
+	public static class P11
+	{
+		public static IEnumerable<int> MaxDistincte(IEnumerable<int> sir)
+		{
+			if (sir == null)
+				throw new ArgumentNullException("sir");
+
+			if (sir.Count() > 100)
+				throw new ArgumentException("Lungimea nu poate fi mai mare decat 100!", "sir");
+			if (sir.Any(element => (element < -30000 || 30000 < element)))
+				throw new ArgumentException("Elementele trebuie sa fie intregi si in intervalul [-30 000, 30 000]!");
+
+			List<int> rezultat = new List<int>();
+			ICollection<int> temp = new List<int>();
+
+			foreach (int element in sir)
+				if (temp.Contains(element))
+				{
+					if (temp.Count > rezultat.Count)
+					{
+						rezultat.Clear();
+						rezultat.AddRange(temp);
+					}
+					temp.Clear();
+				}
+				else
+					temp.Add(element);
+
+			if (temp.Count > rezultat.Count)
+				return temp;
+			else
+				return rezultat;
+		}
+	}
+}
---
+++ Semestrul6/VVSS/L3/Teste/UnitTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace L3.Teste
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod, ExpectedException(typeof(ArgumentNullException))]
+		public void TestNull()
+		{
+			P11.MaxDistincte(null);
+		}
+		[TestMethod, ExpectedException(typeof(ArgumentException))]
+		public void TestMaiMultDecat100()
+		{
+			P11.MaxDistincte(new int[101]);
+		}
+		[TestMethod, ExpectedException(typeof(ArgumentException))]
+		public void TestElementMaiMareDecat30000()
+		{
+			int[] dateDeTest = new int[100];
+			dateDeTest[15193 % dateDeTest.Length] = 30001;
+
+			P11.MaxDistincte(dateDeTest);
+		}
+		[TestMethod, ExpectedException(typeof(ArgumentException))]
+		public void TestElementMaiMicDecat30000()
+		{
+			int[] dateDeTest = new int[100];
+			dateDeTest[93151 % dateDeTest.Length] = -30001;
+
+			P11.MaxDistincte(dateDeTest);
+		}
+
+		[TestMethod]
+		public void TestSecventaVida()
+		{
+			Assert.AreEqual(P11.MaxDistincte(new int[0]).Count(), 0);
+		}
+		[TestMethod]
+		public void TestExact100DeElementeEgale()
+		{
+			Assert.IsTrue(new[] { 0 }.SequenceEqual(P11.MaxDistincte(new int[100])));
+		}
+		[TestMethod]
+		public void TestToateElementeleDistincte()
+		{
+			int[] dateDeTest = new int[50];
+			for (int index = 0; index < dateDeTest.Length; index++)
+				dateDeTest[index] = index;
+
+			Assert.IsTrue(dateDeTest.SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+		[TestMethod]
+		public void TestPrimele10ElementeDistincte()
+		{
+			int[] dateDeTest = new int[50];
+			for (int index = 0; index < dateDeTest.Length; index++)
+				dateDeTest[index] = index % 10;
+
+			Assert.IsTrue(dateDeTest.Take(10).SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+		[TestMethod]
+		public void TestPrimele10ElementeIdenticeUrmatoareleDistincte()
+		{
+			int[] dateDeTest = new int[20];
+			for (int index = 10; index < dateDeTest.Length; index++)
+				dateDeTest[index] = index;
+
+			Assert.IsTrue(dateDeTest.Skip(10).SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+		[TestMethod]
+		public void TestPrimele10ElementeIdenticeUrmatoarele10DistincteUrmatoarele5Identice()
+		{
+			int[] dateDeTest = new int[25];
+			for (int index = 0; index < 10; index++)
+				dateDeTest[10 + index] = index;
+
+			Assert.IsTrue(dateDeTest.Skip(10).Take(10).SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+		[TestMethod]
+		public void TestPrimele10ElementeIdenticeUrmatoarele10DistincteUrmatoarele5IdenticeUrmatoarele10Identice()
+		{
+			int[] dateDeTest = new int[30];
+			for (int index = 0; index < 10; index++)
+				dateDeTest[10 + index] = index;
+			for (int index = 0; index < 5; index++)
+				dateDeTest[25 + index] = index;
+
+			Assert.IsTrue(dateDeTest.Skip(10).Take(10).SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+		[TestMethod]
+		public void TestPrimele10ElementeIdenticeUrmatoarele5DistincteUrmatoarele5IdenticeUrmatoarele10Distincte()
+		{
+			int[] dateDeTest = new int[30];
+			for (int index = 0; index < 5; index++)
+				dateDeTest[10 + index] = index;
+			for (int index = 0; index < 10; index++)
+				dateDeTest[20 + index] = index;
+
+			Assert.IsTrue(dateDeTest.Skip(20).Take(10).SequenceEqual(P11.MaxDistincte(dateDeTest)));
+		}
+	}
+}
